24669fadcc01809cebf3246696e18fdb28feaf60
Tim Besard <tim.besard@gmail.com>
2025-12-17 15:35:17 -0500

    Run examples in parallel.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,34 @@ const init_code = quote
     import cuTile as ct
 end
 
-runtests(cuTile, ARGS; init_code)
+testsuite = find_tests(pwd())
+
+# Add examples
+function find_sources(path::String, sources=String[])
+    if isdir(path)
+        for entry in readdir(path)
+            find_sources(joinpath(path, entry), sources)
+        end
+    elseif endswith(path, ".jl")
+        push!(sources, path)
+    end
+    sources
+end
+examples_dir = joinpath(@__DIR__, "..", "examples")
+examples = find_sources(examples_dir)
+filter!(file -> readline(file) != "# EXCLUDE FROM TESTING", examples)
+for example in examples
+    name = splitext("examples/$(basename(example))")[1]
+    testsuite[name] = quote
+        cd($examples_dir) do
+            mod = @eval module $(gensym()) end
+            @eval mod begin
+                redirect_stdout(devnull) do
+                    include($example)
+                end
+            end
+        end
+    end
+end
+
+runtests(cuTile, ARGS; init_code, testsuite)
