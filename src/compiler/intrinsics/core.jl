9fc8d0a75b14e645d9b8ae5109a4093398473abf
Arhik <arhik23@gmail.com>
2026-01-12 01:13:05 +0530

    fix is_signed to use proper Julia type hierarchy check
    - T <: Integer && !(T <: Unsigned) correctly identifies:
      - Int32, Int64, etc. as signed (true)
      - UInt32, UInt64, etc. as unsigned (false)

--- src/compiler/intrinsics/core.jl
+++ src/compiler/intrinsics/core.jl
@@ -658,7 +658,7 @@ end
 
 Return true if type T is signed, false for unsigned types.
 """
-is_signed(::Type{T}) where T <: Integer = T <: SignedInteger
+is_signed(::Type{T}) where T <: Integer = T <: Integer && !(T <: Unsigned)
 is_signed(::Type{T}) where T <: AbstractFloat = false
 
 """
